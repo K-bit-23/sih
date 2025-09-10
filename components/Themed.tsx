56fa6195a728dbb78140d76f5c284c9698070810
dkarthideepak <dkarthideepak@gmail.com>
2025-09-10 16:36:46 +0000

    n

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -43,7 +43,7 @@ export function View(props: ViewProps) {
 
   const webStyle = Platform.OS === 'web' && {
 	  width: '100%',
-	  height: '100vh'
+	  height: '100%'
   }
 
   return <DefaultView style={[{ backgroundColor }, style, webStyle]} {...otherProps} />;
