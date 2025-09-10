4b93a2362044bd8375d41386c41e802ff7bbee7e
dkarthideepak <dkarthideepak@gmail.com>
2025-09-10 16:25:42 +0000

    jjj

--- app/(admin)/users.tsx
+++ app/(admin)/users.tsx
@@ -1,9 +1,9 @@
 import { Text, View } from 'react-native';
 
-export default function AdminPage() {
+export default function AdminUsersPage() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 24 }}>Admin Page</Text>
+      <Text style={{ fontSize: 24 }}>Admin Users Page</Text>
     </View>
   );
 }
